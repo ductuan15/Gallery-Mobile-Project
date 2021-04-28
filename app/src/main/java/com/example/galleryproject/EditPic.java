011ff4f1d2df8cb028885d929b91f71ac65f8822
Hoàng Xuân Kiệt <kiethoang611@gmail.com>
2021-04-28 22:33:33 +0700

    edit image layout

--- app/src/main/java/com/example/galleryproject/EditPic.java
+++ app/src/main/java/com/example/galleryproject/EditPic.java
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,7 +14,9 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageButton;
 
+import ja.burhanrashid52.photoeditor.PhotoEditor;
 import ja.burhanrashid52.photoeditor.PhotoEditorView;
 
 /**
@@ -21,6 +25,8 @@ import ja.burhanrashid52.photoeditor.PhotoEditorView;
  */
 public class EditPic extends AppCompatActivity {
 
+    ImageButton cropBtn, filterBtn, emojiBtn, brushBtn, textBtn, toneBtn;
+    PhotoEditor mPhotoEditor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,13 +37,31 @@ public class EditPic extends AppCompatActivity {
         PhotoEditorView mPhotoEditorView = findViewById(R.id.photoEditorView);
         mPhotoEditorView.getSource().setImageURI(imageUri);
 
+        Typeface mTextRobotoTf = ResourcesCompat.getFont(this, R.font.roboto_medium);
+
+//loading font from assest
+        Typeface mEmojiTypeFace = Typeface.createFromAsset(getAssets(), "emojione-android.ttf");
+
+        mPhotoEditor = new PhotoEditor.Builder(this, mPhotoEditorView)
+                .setPinchTextScalable(true)
+                .setDefaultTextTypeface(mTextRobotoTf)
+                .setDefaultEmojiTypeface(mEmojiTypeFace)
+                .build();
+
+        cropBtn = findViewById(R.id.crop_button);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.editpicture_menu, menu);
+        menuInflater.inflate(R.menu.editpicture_menu,menu);
         return true;
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
     }
 }
 
